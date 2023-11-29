159467fa686bc8e113076773fb7956eed67ae529
Anusha Antony <52962772+Anushaca1995@users.noreply.github.com>
2023-11-29 12:07:12 +0000

    persist added

--- AppExe/App.js
+++ AppExe/App.js
@@ -10,12 +10,18 @@ import {NavigationContainer} from '@react-navigation/native';
 import Navigator from './src/Navigator';
 import {Provider} from 'react-redux';
 import {store} from './src/store';
+import persistStore from 'redux-persist/es/persistStore';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => {
+  let persistor = persistStore(store);
+
   return (
     <NavigationContainer>
       <Provider store={store}>
-        <Navigator />
+        <PersistGate persistor={persistor}>
+          <Navigator />
+        </PersistGate>
       </Provider>
     </NavigationContainer>
   );
--- AppExe/src/store/index.js
+++ AppExe/src/store/index.js
@@ -1,12 +1,21 @@
 import {configureStore} from '@reduxjs/toolkit';
 import modalReducer from '../reducers/modal/modalSlice';
 import counterReducer from '../reducers/counter/counterSlice';
+import {combineReducers} from '@reduxjs/toolkit';
+import persistReducer from 'redux-persist/es/persistReducer';
+import storage from 'redux-persist/lib/storage';
+
+let persistConfig = {key: 'root', storage};
+
+let rootReducer = combineReducers({
+  modal: modalReducer,
+  counter: counterReducer,
+});
+
+let persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    modal: modalReducer,
-    counter: counterReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export default store;
